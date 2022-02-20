a8f50a6b8a164434093e7db3d4d7c1974de4ad14
Alesyrex <Alesyrex@gmail.com>
2022-02-20 20:48:11 +0300

    create electronic-watch

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -3,10 +3,21 @@ package com.epam.rd.autotasks.meetautocode;
 import java.util.Scanner;
 
 public class ElectronicWatch {
+    public static final int MAXSECOFMIN = 60;
+    public static final int MAXSECOFHOUR = 3600;
+    public static final int MAXSECOFDAY = 86400;
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        seconds = Math.abs(seconds);
+        seconds %= MAXSECOFDAY;
 
-
+        int hour = seconds / MAXSECOFHOUR;
+        int min = (seconds - (hour * MAXSECOFHOUR)) / MAXSECOFMIN;
+        int sec = seconds - (hour * MAXSECOFHOUR) - (min * MAXSECOFMIN);
+        String output = String.format("%d:%02d:%02d", hour,min,sec);
+        System.out.println(output);
     }
 }
