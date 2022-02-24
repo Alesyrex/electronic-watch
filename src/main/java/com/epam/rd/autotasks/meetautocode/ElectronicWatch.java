db454bae04a734f4e3ec11a7033e4b8d3571ceca
Alesyrex <Alesyrex@gmail.com>
2022-02-24 19:06:26 +0300

    correct constant

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -3,21 +3,22 @@ package com.epam.rd.autotasks.meetautocode;
 import java.util.Scanner;
 
 public class ElectronicWatch {
-    public static final int MAXSECOFMIN = 60;
-    public static final int MAXSECOFHOUR = 3600;
-    public static final int MAXSECOFDAY = 86400;
+    public static final int SEC_OF_MIN = 60;
+    public static final int SEC_OF_HOUR = 3600;
+    public static final int SEC_OF_DAY = 86400;
+    public static final String OUTPUT_FORMAT = "%d:%02d:%02d";
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
         seconds = Math.abs(seconds);
-        seconds %= MAXSECOFDAY;
+        seconds %= SEC_OF_DAY;
 
-        int hour = seconds / MAXSECOFHOUR;
-        int min = (seconds - (hour * MAXSECOFHOUR)) / MAXSECOFMIN;
-        int sec = seconds - (hour * MAXSECOFHOUR) - (min * MAXSECOFMIN);
-        String output = String.format("%d:%02d:%02d", hour,min,sec);
+        int hour = seconds / SEC_OF_HOUR;
+        int min = (seconds - (hour * SEC_OF_HOUR)) / SEC_OF_MIN;
+        int sec = seconds - (hour * SEC_OF_HOUR) - (min * SEC_OF_MIN);
+        String output = String.format(OUTPUT_FORMAT, hour,min,sec);
         System.out.println(output);
     }
 }
